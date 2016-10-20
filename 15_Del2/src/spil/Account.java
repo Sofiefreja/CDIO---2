05a25acb95afcd67b8c329221e412060adf9ee97
Jonas <Jonas@10.16.163.233>
2016-10-20 15:21:09 +0200

    Account Class added.

--- 15_Del2/src/spil/Account.java
+++ 15_Del2/src/spil/Account.java
@@ -2,4 +2,22 @@ package spil;
 
 public class Account {
 
+	private int balance;
+
+	public Account() {
+		this.balance = 0;
+	}
+
+	public void deposit(int value) {
+		this.balance = value;
+	}
+
+	public void withdraw(int value) {
+		this.balance -= value;
+
+	}
+	public int getBalance() {
+
+		return balance;
+	}
 }
