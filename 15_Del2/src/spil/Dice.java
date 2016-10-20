fadd9f4a2f9550bbe61d55a9d2af08318ab5f082
Jonas <Jonas@10.16.163.233>
2016-10-20 13:52:40 +0200

    if statementeyes > 0

--- 15_Del2/src/spil/Dice.java
+++ 15_Del2/src/spil/Dice.java
@@ -6,9 +6,15 @@ public class Dice {
 	private int value;
 	private int eyes;
 
-	public Dice(int eyes){
-		this.eyes=eyes;
+	public Dice(int eyes) {
+		if (eyes > 0) {
+			this.eyes = eyes;
+		}
+		else{
+			this.eyes=6;
+		}
 	}
+
 	public int roll() {// roll the die and return the value (1-6)
 		Random dice = new Random();
 		int res = dice.nextInt(eyes) + 1;
