d785a65b80e91906d5399edacea49aa4f5356da5
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-10-20 14:56:06 +0200

    Added methods toString, balance and position.

--- 15_Del2/src/spil/Player.java
+++ 15_Del2/src/spil/Player.java
@@ -4,9 +4,27 @@ public class Player {
 	private String name;
 	private Account balance;
 	private int position;
-	public void Player (String name){
+	public Player (String name){
 		this.name=name;
 		this.balance.deposit(1000);
 		this.position=0;
+	}
+	public String toString(){
+		return name+" has "+getBalance()+" DKK.";
+	}
+	public void addBalance(int amount){
+		balance.deposit(amount);
+	}
+	public void subBalance(int amount){
+		balance.withdraw(amount);
+	}
+	public int getBalance(){
+		return balance.getBalance();
+	}
+	public void setPosition(int position){
+		this.position=position;
+	}
+	public int getPosition(){
+		return position;
 	}
 }
