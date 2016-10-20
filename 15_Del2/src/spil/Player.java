5f6f82a7e5b658a4a0eb242b29b06f85e44f5c2c
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-10-20 14:35:33 +0200

    committed player constructor.

--- 15_Del2/src/spil/Player.java
+++ 15_Del2/src/spil/Player.java
@@ -1,5 +1,12 @@
 package spil;
 
 public class Player {
-
+	private String name;
+	private Account balance;
+	private int position;
+	public void Player (String name){
+		this.name=name;
+		this.balance.deposit(1000);
+		this.position=0;
+	}
 }
