6fd3b5193c0649d4847271df8d7f8131a6f5175e
s147300 <s147300@student.dtu.dk>
2016-10-24 15:39:10 +0200

    JUnit test Account class.

---
+++ 15_Del2/src/test/testAccount.java
@@ -0,0 +1,45 @@
+package test;
+import spil.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAccount {
+	
+	@Test
+	public void testgetBalance() {
+		
+		Account balance = new Account();
+		
+		int a = balance.getBalance();
+		assertEquals(0, a);
+	
+			
+		
+	}
+	@Test
+	public void testDeposit() {
+		
+		Account balance = new Account();
+		
+		balance.deposit(200);
+		
+		int a = balance.getBalance();
+		assertEquals(200, a);
+	}
+	@Test
+	public void testWithdraw() {
+		
+		Account balance = new Account();
+		
+		balance.deposit(200);
+		balance.withdraw(100);
+		
+		int a = balance.getBalance();
+		assertEquals(100, a);
+	}
+	
+	
+
+}
