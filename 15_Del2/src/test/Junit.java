aaf8aa33626e17b54ab62a68eb4f67531c2d4b55
Sofie <Sofie@Sofies_Pc.eduroam.wireless.dtu.dk>
2016-10-24 15:09:02 +0200

    Tests

---
+++ 15_Del2/src/test/Junit.java
@@ -0,0 +1,16 @@
+package test;
+import spil.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit {
+
+	@Test
+	public void test() {
+		Player p = new Player("Børge");
+		
+		fail("Not yet implemented");
+	}
+
+}
