46fc470bfd20a22f14112bcedd0cbedfae400cc3
Simon <Simon@10.16.73.157>
2016-10-31 15:29:39 +0100

    Commented on the code

--- 15_Del2/src/spil/Board.java
+++ 15_Del2/src/spil/Board.java
@@ -8,16 +8,15 @@ import java.util.*;
 
 public class Board {
 
-	public static String extra;
-	public static int done;
+	public static String extra; // String to determine when a user gets an extra turn
 
 	public Board() {
 
-		makeBoard();
+		makeBoard(); // Initiates the makeBoard method, which initializes the GUI for the board, with squares
 		GUI.showMessage("Welcome to the MoneyRace!!!");
-		Player p1 = new Player(GUI.getUserString("Player One, please enter your name:"));
-		Player p2 = new Player(GUI.getUserString("Player Two, please enter your name:"));
-		Dice d1 = new Dice(6);
+		Player p1 = new Player(GUI.getUserString("Player One, please enter your name:")); // Asks the user for a input and then initiates the player class, taking a string (name) as the input, which will be called several times during the code
+		Player p2 = new Player(GUI.getUserString("Player Two, please enter your name:")); // Another initiation of the player class, this time for player two
+		Dice d1 = new Dice(6); // Initiates two die with six eyes.
 		Dice d2 = new Dice(6);
 
 		String cp = "p1";
@@ -25,39 +24,39 @@ public class Board {
 		int posp1 = 1;
 		int posp2 = 1;
 
-		while (done == 0) {
-			do {
-				if (cp.equals("p1")) {
-					boolean res = getUserInput(p1);
 
-					if (res == true) {
-						GUI.setDice(d1.roll(), d2.roll());
-						int resroll = d1.getValue() + d2.getValue();
-						GUI.removeCar(posp1, p1.toString());
-						GUI.setCar(resroll, p1.toString());
+			do { // Initiates a do-while loop, that runs the game until the win conditions are met
+				if (cp.equals("p1")) { // If-else statement that check for which player has a turn
+					boolean res = getUserInput(p1); // Calls the getUserInput method, which requires the user to press one of three buttons to proceed
+
+					if (res == true) { // If the player chose to roll, the turn proceeds
+						GUI.setDice(d1.roll(), d2.roll()); // Both die classes are called to roll, which returns the result of the roll to the GUI
+						int resroll = d1.getValue() + d2.getValue(); // Saves the result of both rolls in one variable
+						GUI.removeCar(posp1, p1.toString()); // Removes the player piece from the current position on the board
+						GUI.setCar(resroll, p1.toString()); // Replaces the player piece on the new position on the board
 						posp1 = resroll;
-						int gold = getFieldInfo(resroll);
-						if (gold > 0) {
+						int gold = getFieldInfo(resroll); // Calls the getFieldInfo method, which returns a value and displays a message to the GUI about which square the player landed on
+						if (gold > 0) { // If-else statement that calls a method from the player class, to either add or subtract points to the player account, depending on the value on the square
 							p1.addBalance(gold);
 						} else {
 							p1.subBalance(gold);
 						}
-						GUI.setBalance(p1.toString(), p1.getBalance());
-						if (extra.equals("Yes")) {
+						GUI.setBalance(p1.toString(), p1.getBalance()); // Sets the new balance to the player car on the GUI
+						if (extra.equals("Yes")) { // If-else statement that checks whether or not the player landed on the extra turn square
 							cp = "p1";
-							extra = "No";
+							extra = "No"; // If the player recieved an extra turn, resets the string to remove the extra turn label, so that only one extra turn is granted
 						} else {
-							cp = "p2";
+							cp = "p2"; // Sets the current player to the next player if no extra turn was recieved
 						}
-						GUI.removeCar(posp1, p1.toString());
-						GUI.setCar(1, p1.toString());
+						GUI.removeCar(posp1, p1.toString()); // Removes the player piece from the new square
+						GUI.setCar(1, p1.toString()); // Sets the player piece to the starting square
 						posp1 = 1;
 					} else {
-						GUI.showMessage("You have surrendered! " + p2 + " is the winner!");
+						GUI.showMessage("You have surrendered! " + p2 + " is the winner!"); // If the player chose not to roll, ends the game and pushes a message to the GUI announcing the winner
 						getEndGameInput(p1);
 						break;
 					}
-				} else {
+				} else { // The following code does the same as for player one
 					boolean res = getUserInput(p2);
 					if (res == true) {
 						GUI.setDice(d1.roll(), d2.roll());
@@ -83,40 +82,30 @@ public class Board {
 						posp2 = 1;
 					} else {
 						GUI.showMessage("You have surrendered! " + p1 + " is the winner!");
-						
 						getEndGameInput(p1);
 						break;
-						
 
 					}
 				}
 			}
 
-			while (p1.getBalance() < 3000 && p2.getBalance() < 3000 && p1.getBalance() > 0 && p2.getBalance() > 0);
-			{
-				if (cp.equals("p1") && p2.getBalance() > 2999 || cp.equals("p2") && p1.getBalance() <= 0) {
-
-					GUI.showMessage("Congratulations " + p2 + "!" + " You are the winner!");
-
-					done = 1;
-					getEndGameInput(p1);
-				} else if (cp.equals("p2") && p1.getBalance() > 2999 || cp.equals("p1") &&p2.getBalance() <= 0) {
-					GUI.showMessage("Congratulations " + p1 + "!" + " You are the winner!");
-
-					done = 1;
+			while (p1.getBalance() < 3000 && p2.getBalance() < 3000 && p1.getBalance() > 0 && p2.getBalance() > 0); { // Win condition that breaks the loop and ends the game if either player gets below 0 or above 3000 points
+				if (cp.equals("p1") && p2.getBalance() > 2999 || cp.equals("p2") && p1.getBalance() <= 0) { // If-else statement checking for which player achieved the win condition
+					GUI.showMessage("Congratulations " + p2 + "!" + " You are the winner!"); // Announces the winner to the GUI
 					getEndGameInput(p1);
 				} else {
-					done = 1;
-
+					GUI.showMessage("Congratulations " + p1 + "!" + " You are the winner!");
+					getEndGameInput(p1);
+	
 				}
 			}
 			
 		}
 		
-	}
+	
 	
 
-	public void makeBoard() {
+	public void makeBoard() { // Method that creates the board for the GUI and sets the squares with their descriptions, colouring and number
 		Field[] fields = new Field[12];
 		fields[0] = new Street.Builder().setTitle("Begin").setDescription("This is where the game begins.")
 				.setSubText(" ").setBgColor(Color.yellow).setFgColor(Color.green).build();
@@ -148,7 +137,7 @@ public class Board {
 
 	}
 
-	public int getFieldInfo(int t) {
+	public int getFieldInfo(int t) { // Method that pushes a message to the GUI telling the player what happens on the square they landed on, while returning the square's value
 		int res = 0;
 		switch (t) {
 		case 2:
@@ -200,13 +189,12 @@ public class Board {
 		return res;
 	}
 
-	public boolean getUserInput(Player p) {
+	public boolean getUserInput(Player p) { // Method that pushes a menu to the GUI, expecting an input from the player to either roll the die, read the rules or surrender the game
 		boolean roll = false;
 		boolean proceed = false;
 		String input;
 		do {
-			input = GUI.getUserButtonPressed(p.toString() + ", it is your turn. Take an action:", "Roll",
-					"Display balance", "Display rules", "Surrender");
+			input = GUI.getUserButtonPressed(p.toString() + ", it is your turn. Take an action:", "Roll", "Display rules", "Surrender");
 			if (input.equals("Roll")) {
 				proceed = true;
 				roll = true;
@@ -216,10 +204,10 @@ public class Board {
 				proceed = true;
 			}
 		} while (proceed == false);
-		return roll;
+		return roll; // Returns roll = true if the player rolled the die, false if the player surrenders
 	}
 
-	public boolean getEndGameInput(Player p) {
+	public boolean getEndGameInput(Player p) { // Method that ends the game, telling the player to relaunch if they wish to play again
 		String input;
 
 			GUI.showMessage("The game has ended. To play again, relaunch the game.");
