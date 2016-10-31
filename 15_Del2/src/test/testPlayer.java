7b180c7a862846dd8df0162f3da487d894aae81c
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2016-10-31 14:29:16 +0100

    fixed testPlayer

--- 15_Del2/src/test/testPlayer.java
+++ 15_Del2/src/test/testPlayer.java
@@ -10,14 +10,13 @@ public class testPlayer {
 		//Test name function
 				Player p = new Player("Svend");
 				assertEquals("Svend",p.toString());
-				//Test position function & setPosition
 				assertEquals(0, p.getPosition());
 				p.setPosition(5);
 				assertEquals(5,p.getPosition());
 				//Test getBalance function
 				assertEquals(1000, p.getBalance());
 				//Test withdraw and deposit function
-				p.subBalance(200);
+				p.subBalance(-200);
 				assertEquals(800,p.getBalance());
 				p.addBalance(650);
 				assertEquals(1450,p.getBalance());
